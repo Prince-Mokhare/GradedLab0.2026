8ff9e7ab42689139c2e8a9db06107c9a011e67ce
Prince <princemokhare700@gmail.com>
2026-02-18 20:18:31 +0200

    Initial commit: setup taskboard project, add index.html, verify Node/npm, add hello-node script and README

---
+++ taskboard/README.md
@@ -0,0 +1,9 @@
+# Taskboard Project
+
+## Tool Versions
+
+Node version:
+v24.13.1
+
+npm version:
+11.8.0
---
+++ taskboard/hello-node.js
@@ -0,0 +1,7 @@
+const data = {
+    name: "Prince",
+    project: "Taskboard",
+    status: "Toolchain setup complete"
+};
+
+console.log(JSON.stringify(data, null, 2));
